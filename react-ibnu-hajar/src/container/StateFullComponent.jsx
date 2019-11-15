042d939cf9be6a45bc3d46aaee1b72e65c9c09e4
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2019-11-15 21:32:41 +0700

    fix problem undifine P at file StateFullComponent

--- react-ibnu-hajar/src/container/StateFullComponent.jsx
+++ react-ibnu-hajar/src/container/StateFullComponent.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 class StateFullComponent extends React.Component
 {
     render(){
-        // eslint-disable-next-line react/jsx-no-undef
-        return <P>ibnuhajar</P>
+        return <p>ibnuhajar</p>
     }
 }
 
--- react-ibnu-hajar/src/index.js
+++ react-ibnu-hajar/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import HelloComponent from './component/HelloComponent';
-ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+// import HelloComponent from './component/HelloComponent';
+import StateFullComponent from "./container/StateFullComponent";
+ReactDOM.render( < StateFullComponent / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
