ef59c0ee3625ba8c64a22de1aaefc10699fb600d
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2019-11-15 22:43:40 +0700

    change from p to h1

--- react-ibnu-hajar/src/component/HelloComponent.jsx
+++ react-ibnu-hajar/src/component/HelloComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HelloComponent.css';
 
 const HelloComponent = () => {
-    return <p class="text-p">ibnu hajar</p>;
+    return <h1 className="text-p">ibnu</h1>
 }
 
 export default HelloComponent;
