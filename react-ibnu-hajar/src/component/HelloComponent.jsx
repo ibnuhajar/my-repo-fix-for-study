47028852401bedc652bb2d9dbce7ad5cf55ca40f
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2019-11-15 22:44:29 +0700

    not change

--- react-ibnu-hajar/src/component/HelloComponent.jsx
+++ react-ibnu-hajar/src/component/HelloComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './HelloComponent.css';
 
 const HelloComponent = () => {
-    return <h1 className="text-p">ibnu</h1>
+    return <h1 className="text-p"> ibnu </h1>
 }
 
 export default HelloComponent;
