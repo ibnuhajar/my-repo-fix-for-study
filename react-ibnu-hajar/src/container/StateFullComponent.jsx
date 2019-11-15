6d506eb9e4ad15e5bb17f136c9ebdcbbe9b9362d
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2019-11-15 21:24:46 +0700

    first create StateFullcomponent react at folder container

---
+++ react-ibnu-hajar/src/container/StateFullComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class StateFullComponent extends React.Component
+{
+    render(){
+        // eslint-disable-next-line react/jsx-no-undef
+        return <P>ibnuhajar</P>
+    }
+}
+
+
+export default StateFullComponent;
