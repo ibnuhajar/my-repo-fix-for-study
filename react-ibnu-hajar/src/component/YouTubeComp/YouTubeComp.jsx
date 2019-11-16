92654b85856e71ea304aaa5fc3ff031df116809d
Ibnu Hajar <ibnuhajardewantara62@gmail.com>
2019-11-16 21:19:46 +0700

    gabut commit nya

---
+++ react-ibnu-hajar/src/component/YouTubeComp/YouTubeComp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./YouTubeComp.css";
+
+const YouTubeComp = () => {
+    return (
+        <div>
+            <p>ibnu hajar</p>
+            <p>hajar ibnu </p>
+        </div>
+    );
+
+}
+
+export default YouTubeComp;
--- react-ibnu-hajar/src/index.js
+++ react-ibnu-hajar/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import HelloComponent from './component/HelloComponent';
+// import HelloComponent from './component/HelloComponent';
+import YouTubeComp from './component/YouTubeComp/YouTubeComp';
 // import StateFullComponent from "./container/StateFullComponent";
-ReactDOM.render( < HelloComponent / > , document.getElementById('root'));
+ReactDOM.render( <YouTubeComp / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
